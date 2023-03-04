e93837b761ccba2d3354ffcf083f629539b34807
Franco Ezequiel Rodriguez <frrodriguez@fi.uba.ar>
2023-03-04 16:09:45 -0300

    Correcciones

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -9,16 +9,15 @@ public class Main {
 
     public static void main(String[] args) throws IOException {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        System.out.println(resultado(10, 40.07, reader));
+        System.out.println(resultado(12, 8.5975, reader));
     }
 
     public static double resultado(int cantidad, double promedio, BufferedReader reader) throws IOException {
         double res = 0;
         for(int i = 0; i < cantidad; i++){
             double aux = Float.parseFloat(reader.readLine());
-            System.out.println(aux-promedio);
             res += Math.pow(aux - promedio, 2);
         }
-        return res/(cantidad - 1);
+        return res/(cantidad - 1); //Devuelve S^2 (es la cuenta que se realiza en los metodos)
     }
 }
