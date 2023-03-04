4ff26caa30960d95cf24b97ee421faccc6b2d298
Franco Ezequiel Rodriguez <frrodriguez@fi.uba.ar>
2023-03-04 20:40:54 -0300

    Calculo de promedio automatico, la variable S se calcula sin estar elevada al cuadrado

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,20 +4,35 @@ package org.example;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
 
     public static void main(String[] args) throws IOException {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        System.out.println(resultado(12, 8.5975, reader));
+        List<Double> list = new ArrayList<>();
+        System.out.println("El valor de S es: " + resultado(10, list, reader));
     }
 
-    public static double resultado(int cantidad, double promedio, BufferedReader reader) throws IOException {
+    public static double resultado(int cantidad, List<Double> list, BufferedReader reader) throws IOException {
+        double prom;
         double res = 0;
         for(int i = 0; i < cantidad; i++){
             double aux = Float.parseFloat(reader.readLine());
-            res += Math.pow(aux - promedio, 2);
+            list.add(aux);
         }
-        return res/(cantidad - 1); //Devuelve S^2 (es la cuenta que se realiza en los metodos)
+        prom = promedio(list);
+        for (Double aDouble : list) {
+            res += Math.pow(aDouble - prom, 2);
+        }
+        return Math.sqrt(res/(cantidad - 1));
+    }
+
+
+    public static double promedio(List<Double> list){
+        return list.stream()
+                .mapToDouble(a -> a)
+                .average().orElse(0);
     }
 }
